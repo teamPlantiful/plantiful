e4dad86058117345036081608a803c0c87d66fe1
Hyunji <hyunhyun0096@naver.com>
2025-11-07 14:13:52 +0900

    feat : 식물 검색 컴포넌트 구현 (#70)
    * feat: 직접 입력란 추가
    
    * feat: 식물 검색란 추가
    
    * feat: 식물 목록란 추가
    
    * feat: 식물 검색 모달 추가
    
    * 식물 검색 커스텀 훅 추가
    
    * hooks 수정
    
    * 커서포인터 추가

---
+++ src/components/PlantSearchInput.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Input } from '../components/common/Input'
+import { Search } from 'lucide-react'
+
+interface PlantSearchInputProps {
+  value: string
+  onChange: (value: string) => void
+}
+
+export default function PlantSearchInput({ value, onChange }: PlantSearchInputProps) {
+  return (
+    <div className="relative px-4">
+      <Search className="absolute left-7 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+      <Input
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="식물 이름 검색..."
+        size="md"
+        className="pl-10 bg-muted/30 border-none"
+      />
+    </div>
+  )
+}
