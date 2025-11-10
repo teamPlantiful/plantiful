fe1431ee0d79d43071bbf73bbbe94a4845e588ca
asdfdk123 <cwh0607@naver.com>
2025-11-10 16:10:21 +0900

    page 오류 해결

---
+++ src/app/(main)/auth/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Register() {
+  return (
+    <div className="flex flex-col items-center">
+      <p className="text-sm sm:text-base lg:text-lg">
+        아직 구현 중입니다. 다음 버전을 기대해주세요!
+      </p>
+      <Link className="text-xs sm:text-sm lg:text-base text-[#006699] hover:underline" href="/">
+        홈으로 돌아가기
+      </Link>
+    </div>
+  )
+}
