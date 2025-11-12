56f704b681f1d13df2e707a93194e5d26b19783e
Ikmyeong <boroson@naver.com>
2025-11-12 17:19:32 +0900

    feat: 로그아웃 기능 구현

---
+++ src/components/auth/logoutButton.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/store/useAuthStore';
+import { LogOut } from 'lucide-react';
+import Button from '@/components/common/button';
+
+export default function LogoutButton() {
+  const router = useRouter();
+  const signOut = useAuthStore((userState) => userState.signOut);
+
+  // 슈퍼베이스와 연동된 zustand를 이용해서 로그아웃 후 로그인 페이지로 돌아가는 핸들러
+  const logout = async () => {
+    await signOut();
+    router.push('/login');
+  };
+
+  return (
+    <Button
+      variant="outline"
+      className="w-full border-destructive bg-destructive-foreground text-destructive hover:bg-destructive/10"
+      onClick={logout}
+    >
+      <LogOut className="h-4 w-4 mr-2" />
+      로그아웃
+    </Button>
+  );
+}
