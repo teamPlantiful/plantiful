38feb3632d82fedcb252b70e867174971fb7bacf
Ikmyeong <boroson@naver.com>
2025-11-12 17:10:34 +0900

    Feat: 인증 상태 관리 적용

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,11 @@
 import Header from '@/components/Header'
-import TodayPlantSection from '@/components/home/TodayPlantSection'
-import PlantListSection from '@/components/home/PlantListSection'
-import PlantRegistrationFab from '@/components/home/PlantRegistrationFab'
+import Dashboard from './dashboard'
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="max-w-xl mx-auto p-4 space-y-6 md:space-y-8 animate-fade-in">
-        <TodayPlantSection />
-        <PlantListSection />
-        <PlantRegistrationFab />
-      </div>
+      <Dashboard />
     </>
   )
 }
