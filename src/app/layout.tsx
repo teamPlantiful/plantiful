d1ef79b106b68003619f631ef3c680ebd6343e55
Nahyun <from.owol@gmail.com>
2025-11-12 14:13:31 +0900

    feat: setup query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import './globals.css'
+import QueryProvider from '@/providers/query-provider'
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="ko">
       <body>
-        {children}
+        <QueryProvider>
+          {children}
+        </QueryProvider>
       </body>
     </html>
   )
