050301e02feaf59febfdba47a12ac227de496938
Hyunji <hyunhyun0096@naver.com>
2025-11-20 14:47:13 +0900

    refactor

--- src/components/plant/search/PlantSearchInput.tsx
+++ src/components/plant/search/PlantSearchInput.tsx
@@ -15,7 +15,7 @@ export default function PlantSearchInput({ value, onChange }: PlantSearchInputPr
       <Input
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        placeholder="식물 이름 검색..."
+        placeholder="식물 이름을 검색하세요"
         size="md"
         className="pl-10 bg-muted/30 border-none"
       />
