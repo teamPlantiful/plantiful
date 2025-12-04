a7ef35ac25ecc9d21c0bfe525792d2d54d424164
Ikmyeong <boroson@naver.com>
2025-12-04 11:10:17 +0900

    fix: 이메일 중복 검사 미작동 문제 수정

--- src/app/actions/auth/register.ts
+++ src/app/actions/auth/register.ts
@@ -1,18 +1,42 @@
 'use server'
 
-import { cookies } from 'next/headers'
 import { createClient } from '@/utils/supabase/server'
+import { createClient as createAdminClient } from '@supabase/supabase-js'
 
 export async function register(formData: FormData) {
-  // 폼에서 회원가입 데이터 받아오기
+
   const name = (formData.get('name') as string)?.trim()
   const email = formData.get('email') as string
   const password = formData.get('password') as string
   
   const userName = name === "" ? "식집사" : name
 
-  // 서버 측 클라이언트 생성 후, 회원가입 진행
+  // 중복 검사용 admin 클라이언트 생성
+  const supabaseAdmin = createAdminClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false
+      }
+    }
+  )
+
+  // admin 클라이언트로 테이블 비교 후, 중복 검사 실시.
+  const { data: existingUser } = await supabaseAdmin
+    .from("profiles")
+    .select("id")
+    .eq("email", email)
+    .maybeSingle()
+
+  if (existingUser) {
+    return { error: "이미 등록된 이메일입니다." }
+  }
+
+  // 중복 없으면 가입 진행 후 인증 메일 전송
   const supabase = await createClient()
+
   const { error: signUpError } = await supabase.auth.signUp({
     email,
     password,
@@ -25,22 +49,8 @@ export async function register(formData: FormData) {
   })
 
   if (signUpError) {
-    if (signUpError.message.includes("User already registered")) {
-      return { error: "이미 등록된 이메일입니다." }
-    }
     return { error: "회원가입 중 문제가 발생했습니다." }
   }
 
-  const responseCookies = await cookies()
-  responseCookies.set({
-    name: "pending_email",
-    value: email,
-    httpOnly: true,
-    secure: true,
-    path: "/",
-    maxAge: 60 * 10,
-  })
-
-  console.log("signUpError:", signUpError)
   return { success: "이제 이메일 인증을 진행해 주세요!" }
 }
