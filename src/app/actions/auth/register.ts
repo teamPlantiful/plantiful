0fec8370a1943adadce537fdfe305569214c68e2
Ikmyeong <boroson@naver.com>
2025-12-08 15:03:32 +0900

    hotfix: 이메일 인증 오류, 콜백 오류 수정

--- src/app/actions/auth/register.ts
+++ src/app/actions/auth/register.ts
@@ -11,37 +11,13 @@ export async function register(formData: FormData) {
   
   const userName = name === "" ? "식집사" : name
 
-  // 중복 검사용 admin 클라이언트 생성
-  const supabaseAdmin = createAdminClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    {
-      auth: {
-        autoRefreshToken: false,
-        persistSession: false
-      }
-    }
-  )
-
-  // admin 클라이언트로 테이블 비교 후, 중복 검사 실시.
-  const { data: existingUser } = await supabaseAdmin
-    .from("profiles")
-    .select("id")
-    .eq("email", email)
-    .maybeSingle()
-
-  if (existingUser) {
-    return { error: "이미 등록된 이메일입니다." }
-  }
-
-  // 중복 없으면 가입 진행 후 인증 메일 전송
   const supabase = await createClient()
 
   const { error: signUpError } = await supabase.auth.signUp({
     email,
     password,
     options: {
-      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/apis/auth/callback`,
+      emailRedirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/apis/auth/callback`,
       data: {
         name: userName
       }
